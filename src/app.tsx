21f5e658dae1f2d5ee501a097793b9874fdad1af
rrharvey <rharvey@intertech.com>
2017-05-15 14:33:51 -0500

    use functions and multiple children

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,22 @@
+interface Name {
+    firstName: string,
+    lastName?: string
+}
+
+const me = {
+    firstName: 'Ryan',
+    lastName: 'Harvey'
+};
+
+// const formatName = (name: Name) => `${name.firstName} ${name.lastName}`;
+function formatName(name: Name) {
+    return name.firstName + ' ' + name.lastName;
+}
+
 ReactDOM.render(
-    React.createElement('div', {className: 'greeting'}, 'Hello Ryan'),
+    React.createElement('div', {className: 'greeting'},
+        'Hello ',
+        formatName(me)
+    ),
     document.getElementById('app-root')
 );
